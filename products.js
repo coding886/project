49f1efac9730ca0e946a424932d6ccdbf6606126
coding886 <mmanigandan233@gmail.com>
2025-05-23 22:12:33 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,87 @@
+const products = [
+  {
+    id: 1,
+    name: "Camera",
+    description: "Sony Alpha 6600 Mirrorless Digital SLR Camera",
+    price: 699.99,
+    image: "images/products/camera.jpg"
+  },
+  {
+    id: 2,
+    name: "Printer",
+    description:"Epson EcoTank L8050 Ink Tank Photo Printer",
+    price: 649.99,
+    image: "images/products/printer.jpg"
+  },
+  {
+    id: 3,
+    name: "electric fan",
+    description: "Orient Electric Zeno 1200mm BLDC Ceiling Fan",
+    price: 299.99,
+    image: "images/products/fan.jpg"
+  },
+  {
+    id: 4,
+    name: "iron box",
+    description: "Fabripress Non-Stick Iron Box",
+    price: 199.99,
+    image: "images/products/ironbox.jpg"
+  },
+  {
+    id: 5,
+    name: "USB PenDrive",
+    description: "High-quality bass, storage device.",
+    price: 89.99,
+    image: "images/products/pend.jpgrive.jpg"
+  },
+  {
+    id: 6,
+    name: "Oven",
+    description: "Wonderchef Crimson Edge Oven Toaster Griller",
+    price: 499.99,
+    image: "images/products/ovan.jpg"
+  },
+  {
+    id: 7,
+    name: "Projector",
+    description:"WZATCO Yuva Go Android 13.0 Smart Projector",
+    price: 149.99,
+    image: "images/products/projector.jpg"
+  },
+  {
+    id: 8,
+    name: "Speaker",
+    description: "high-quality , loud , voice control ",
+    price: 499.99,
+    image: "images/products/speaker.jpg"
+  },
+  {
+    id: 9,
+    name: "4K Ultra HD Smart TV",
+    description: "55-inch 4K display with HDR, smart apps, and voice control.",
+    price: 799.99,
+    image: "images/products/television.jpg"
+  },
+  {
+    id: 10,
+    name: "Washing machine",
+    description: "High-quality bass,faster washes cleanly.",
+    price: 89.99,
+    image: "images/products/washing.jpg"
+  },
+  {
+    id: 11,
+    name: "Mouse",
+    description: "wireless mouse,usefuldevice",
+    price: 89.99,
+    image: "images/products/mouse.jpg"
+  },
+  {
+    id: 12,
+    name: "Bluetooth Speaker Max",
+    description: "High-quality bass, waterproof, and 10-hour playtime.",
+    price: 89.99,
+    image: "images/products/home.jpg"
+  }
+];
+
